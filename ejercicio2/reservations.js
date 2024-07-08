7d349ed54f944418796d873620364a06251f7680
Nelson Solano <rafasolano2011@gmail.com>
2024-07-07 22:32:35 -0300

    Resolucion del Punto 2

--- ejercicio2/reservations.js
+++ ejercicio2/reservations.js
@@ -1,6 +1,37 @@
-class Customer {}
+// Primer paso trabajamos en la clase Customer
+class Customer {
+    // Definimos el constructor para la clase
+    constructor(id, name, email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+    // Creamos la propieda info para retornar el nombre y email del cliente
+    get info() {
+        return `${this.name} (${this.email})`;
+    }
+}
 
-class Reservation {}
+// Segundo paso trabajamos en la clase Reservation
+class Reservation {
+    // Definimos el constructor para la clase
+    constructor(id, customer, date, guests) {
+        this.id = id;
+        this.customer = customer;
+        this.date = new Date(date);
+        this.guests = guests;
+    }
+    // Definimos la propiedad info para mostrar los datos de las reservas realizadas
+    get info() {
+        return `Date: ${this.date.toLocaleString()}, Customer: ${this.customer.info}, Guests: ${this.guests}`;
+    }
+    // Metodo para validar que la reserva sea correcta, tanto en fecha como en cantidad de personas
+    static validateReservation(date, guests) {
+        const reservationDate = new Date(date);
+        const currentDate = new Date();
+        return reservationDate > currentDate && guests > 0;
+    }
+}
 
 class Restaurant {
     constructor(name) {
